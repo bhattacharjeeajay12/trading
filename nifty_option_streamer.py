4f3615882c53ad0e487906de06e54c168f701cf9
Ajay Bhattacharjee <bhattacharjeeajay12@gmail.com>
2026-03-19 10:59:55 +0530

    Updated Nifty Options Streamer.

--- nifty_option_streamer.py
+++ nifty_option_streamer.py
@@ -26,7 +26,7 @@ OPTION_DEPTH = 2
 OPTION_EXPIRY = None  # Example: "2024-03-28" or None
 
 # Market end time (IST) - streamer will auto-stop at this time
-MARKET_END_TIME = "10:53"  # Format: HH:MM e.g. 15:35; 3:35 PM IST (5 minutes after market close),
+MARKET_END_TIME = "11:04"  # Format: HH:MM e.g. 15:35; 3:35 PM IST (5 minutes after market close),
 
 # ============================================================================
 
