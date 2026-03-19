8289cc2b2ad83f233fb1fa276280f97eefd03f15
Ajay Bhattacharjee <bhattacharjeeajay12@gmail.com>
2026-03-19 08:53:20 +0530

    time zone updated.

--- run_login.sh
+++ run_login.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export TZ='Asia/Kolkata'
 source /root/trading/venv/bin/activate
 cd /root/trading
 python login.py >> /root/trading/login.log 2>&1
--- run_streamer.sh
+++ run_streamer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export TZ='Asia/Kolkata'
 source /root/trading/venv/bin/activate
 cd /root/trading
 python nifty_option_streamer.py >> /root/trading/streamer.log 2>&1
